9e0863a8585ab1810606c713b62c1ab9807b2c3a
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-30 15:42:00 -0700

    321. Cambiar el mensaje de la etiqueta

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './product-page.component.html',
@@ -10,13 +11,22 @@ export class ProductPageComponent {
   private fb = inject( FormBuilder );
 
   public color: string = 'green';
+  public interval$?: Subscription;
+  public flag: number = 0;
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.minLength(6), Validators.email ] ]
   })
 
   changeColor() {
-    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));  
+    if ( this.flag === 0 ) {
+      this.interval$ = interval(500).subscribe( () => this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16)) );
+      this.flag++;
+      return;
+    }
+    this.interval$?.unsubscribe();
+    this.flag = 0;
+    return;
   }
 
 
